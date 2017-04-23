2519dfd692bdb8428ef08911a2bce7d347749ef7
CapitanRedBeard <eharpe23@yahoo.com>
2017-04-23 00:04:18 -0700

    :ambulance: keep the internet weird
    Signed-off-by: CapitanRedBeard <eharpe23@yahoo.com>

--- src/components/AnimatedText/AnimatedText.js
+++ src/components/AnimatedText/AnimatedText.js
@@ -8,26 +8,19 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Layout.css';
-import Header from '../Header';
-import Footer from '../Footer';
+import s from './AnimatedText.css';
+import Link from '../Link';
 
-class Layout extends React.Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  };
-
+class AnimatedText extends React.Component {
   render() {
+    const text = this.props.text || "test";
     return (
-      <div>
-        <Header />
-        {this.props.children}
-        <Footer />
+      <div className={s.root}>
+        {text}
       </div>
     );
   }
 }
 
-export default withStyles(s)(Layout);
+export default withStyles(s)(AnimatedText);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,20 +12,27 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
 import Navigation from '../Navigation';
+import AnimatedText from '../AnimatedText';
 
 class Header extends React.Component {
   render() {
     return (
       <div className={s.root}>
         <Navigation />
+        {
+          // <div className={s.container}>
+          //   <Link className={s.brand} to="/">
+          //     <span className={s.brandTxt}>Home</span>
+          //   </Link>
+          //   <div className={s.banner}>
+          //     <h1 className={s.bannerTitle}>Emmett</h1>
+          //     <p className={s.bannerDesc}>Complex web apps made easy</p>
+          //   </div>
+          // </div>
+        }
         <div className={s.container}>
-          <Link className={s.brand} to="/">
-            <span className={s.brandTxt}>Home</span>
-          </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>Emmett</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
+          <AnimatedText text="Beardo" />
+          <h2 className={s.subtitle}>keep make the internet weird</h2>
         </div>
       </div>
     );
